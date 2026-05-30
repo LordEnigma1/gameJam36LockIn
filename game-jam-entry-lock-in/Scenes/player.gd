c36994cd2a96acd12ffdb9ce0297863c76145d29
LordEnigma1 <angelm.181306@gmail.com>
2026-05-30 10:42:12 -0600

    Created dash mechanic

--- game-jam-entry-lock-in/Scenes/player.gd
+++ game-jam-entry-lock-in/Scenes/player.gd
@@ -5,10 +5,22 @@ extends CharacterBody2D
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
+#dash mechanic
+const DASH_SPEED: float = 450.0
+const DASH_DURATION: float = 0.2
+var is_dashing: bool = false
+var dash_direction: float = 1.0
+@onready var dash_timer: Timer = $dashTimer
+
+
+
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	if is_dashing:
+		velocity.x
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
