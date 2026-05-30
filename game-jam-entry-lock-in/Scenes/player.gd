62cb3818de73eb2f5868574b2ce08bc7d763de47
LordEnigma1 <angelm.181306@gmail.com>
2026-05-30 13:48:18 -0600

    Removed unnecessary files.

--- game-jam-entry-lock-in/Scenes/player.gd
+++ game-jam-entry-lock-in/Scenes/player.gd
@@ -1,63 +1,32 @@
-extends CharacterBody2D
+extends Node2D
 
-@export var speed: float = 300.0
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var blob_body = $"." # Assuming this script is on your Blob's CharacterBody2D
+@onready var possession_area = $PossessionArea
+@onready var camera = $Camera2D # The camera currently following the blob
 
-var gravity: float = 500.0
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("possess"): # Map this in Project Settings
+		try_possess()
 
-# Dash variables
-const DASH_SPEED: float = 1000.0
-var is_dashing: bool = false
-var can_dash: bool = true
-@onready var dash_timer: Timer = $dashTimer
-@onready var dash_cooldown: Timer = $dashCooldown
-
-# Jump variables
-@export var jump_speed: float = -550.0          
-@export var base_gravity: float = 1400.0        
-@export var fall_gravity_multiplier: float = 1.7 
-@export var jump_cut_multiplier: float = 0.35   
-var is_jumping: bool = false
-
-func _physics_process(delta: float) -> void:
-	if not is_on_floor() or velocity.y < 0:
-		var current_gravity = base_gravity
-		
-		if velocity.y > 0:
-			current_gravity *= fall_gravity_multiplier
+func try_possess() -> void:
+	# Get all bodies currently inside our possession radius
+	var overlapping_bodies = possession_area.get_overlapping_bodies()
+	
+	for body in overlapping_bodies:
+		if body.is_in_group("possessable") and body != blob_body:
+			execute_possession(body)
+			return # Stop after possessing the first valid target
 			
-		velocity.y += current_gravity * delta
-	else:
-		is_jumping = false
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_speed
-		is_jumping = true
-
-	if Input.is_action_just_released("jump") and is_jumping and velocity.y < 0:
-		velocity.y *= jump_cut_multiplier
-		is_jumping = false
-
-	# Dash
-	if Input.is_action_just_pressed("dash") and can_dash:
-		is_dashing = true
-		can_dash = false
-		dash_timer.start()
-		dash_cooldown.start()
-
-	# Movement
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * (DASH_SPEED if is_dashing else speed)
-		animated_sprite_2d.flip_h = (direction < 0)
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-
-	move_and_slide()
-
-# Dash functions
-func _on_dash_timer_timeout() -> void:
-	is_dashing = false
-
-func _on_dash_cooldown_timeout() -> void:
-	can_dash = true
+func execute_possession(target_npc) -> void:
+	# 1. Turn off the blob
+	blob_body.is_possessed = false
+	blob_body.visible = false
+	blob_body.set_collision_layer_value(1, false) # Disable blob collision
+	blob_body.set_collision_mask_value(1, false)
+	
+	# 2. Move the camera to the NPC
+	camera.reparent(target_npc)
+	camera.position = Vector2.ZERO # Center camera on the NPC
+	
+	# 3. Give control to the NPC
+	target_npc.is_possessed = true
---
+++ game-jam-entry-lock-in/Scenes/possessed.gd
@@ -0,0 +1,81 @@
+extends CharacterBody2D
+
+@export var is_possessed: bool = false
+@export var speed: float = 300.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var gravity: float = 500.0
+
+
+# Dash variables
+const DASH_SPEED: float = 1000.0
+var is_dashing: bool = false
+var can_dash: bool = true
+@onready var dash_timer: Timer = $dashTimer
+@onready var dash_cooldown: Timer = $dashCooldown
+
+# Jump variables
+@export var jump_speed: float = -550.0          
+@export var base_gravity: float = 1400.0        
+@export var fall_gravity_multiplier: float = 1.7 
+@export var jump_cut_multiplier: float = 0.35   
+var is_jumping: bool = false
+
+func _physics_process(delta: float) -> void:
+	# 1. GATHER INPUTS
+	var input_dir: float = 0.0
+	var want_jump: bool = false
+	var want_jump_release: bool = false
+	var want_dash: bool = false
+
+	if is_possessed:
+		input_dir = Input.get_axis("left", "right")
+		want_jump = Input.is_action_just_pressed("jump")
+		want_jump_release = Input.is_action_just_released("jump")
+		want_dash = Input.is_action_just_pressed("dash")
+	else:
+		pass # Future NPC AI goes here (e.g., set input_dir to wander around)
+
+	# 2. APPLY PHYSICS (Using the variables, not the Input singleton)
+	
+	if not is_on_floor() or velocity.y < 0:
+		var current_gravity = base_gravity
+		if velocity.y > 0:
+			current_gravity *= fall_gravity_multiplier
+		velocity.y += current_gravity * delta
+	else:
+		is_jumping = false
+
+	# Jump
+	if want_jump and is_on_floor():
+		velocity.y = jump_speed
+		is_jumping = true
+
+	# Variable jump height
+	if want_jump_release and is_jumping and velocity.y < 0:
+		velocity.y *= jump_cut_multiplier
+		is_jumping = false
+
+	# Dash
+	if want_dash and can_dash:
+		is_dashing = true
+		can_dash = false
+		dash_timer.start()
+		dash_cooldown.start()
+
+	# Movement
+	if input_dir != 0:
+		velocity.x = input_dir * (DASH_SPEED if is_dashing else speed)
+		animated_sprite_2d.flip_h = (input_dir < 0)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
+
+# Dash functions
+func _on_dash_timer_timeout() -> void:
+	is_dashing = false
+
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true
